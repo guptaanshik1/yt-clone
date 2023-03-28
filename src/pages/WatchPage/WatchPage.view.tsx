ea99e32d13216ebe21bfe1f07101ebb78ec37514
Anshik <guptaanshik1@gmail.com>
2023-03-29 00:33:03 +0530

    setup for related content

--- src/pages/WatchPage/WatchPage.view.tsx
+++ src/pages/WatchPage/WatchPage.view.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch } from "../../app/hooks";
 import { closeSidebar, openSidebar } from "../../features/sidebarSlice";
 import VideoDetails from "./components/VideoDetails";
 import VideoPlayer from "./components/VideoPlayer";
+import Container from "./components/VideoRelatedContent/Container";
 
 export default function WatchPageView() {
   const dispatch = useAppDispatch();
@@ -18,6 +19,7 @@ export default function WatchPageView() {
     <>
       <VideoPlayer />
       <VideoDetails />
+      <Container />
     </>
   );
 }
---
+++ src/pages/WatchPage/components/VideoRelatedContent/ContentCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContentCard = ({ ...video }) => {
+  return <div>ContentCard</div>;
+};
+
+export default ContentCard;
