97dc6de595bf02c7fe48e121f449225bb97e9e7c
Anshik <guptaanshik1@gmail.com>
2023-05-19 01:36:04 +0530

    done setting latest video tab

--- src/pages/Channel/components/VideosTab.tsx
+++ src/pages/Channel/components/VideosTab.tsx
@@ -1,7 +1,25 @@
+import { Button, Flex } from "@chakra-ui/react";
 import React from "react";
+import { useChannelContext } from "../utils/context";
+import ChannelVideoCard from "./ChannelVideoCard";
 
 const VideosTab = () => {
-  return <div>VideosTab</div>;
+  const { channelVideosLatest } = useChannelContext();
+
+  return (
+    <Flex flexDir={"column"} w={"100%"}>
+      <Flex gridGap={"20p"}>
+        <Button>Latest</Button>
+        <Button>Popular</Button>
+      </Flex>
+      {
+        // @ts-ignore
+        channelVideosLatest?.contents?.map((video) => (
+          <ChannelVideoCard key={video?.video?.videoId} video={video?.video} />
+        ))
+      }
+    </Flex>
+  );
 };
 
 export default VideosTab;
