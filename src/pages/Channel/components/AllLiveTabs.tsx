fa8cd74f952cdec9165fc5e6d9bedd1c2b42b022
Anshik <guptaanshik1@gmail.com>
2023-05-25 00:11:51 +0530

    setted live tabs content

---
+++ src/pages/Channel/components/AllLiveTabs.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface IProps {
+  data: any;
+}
+
+const AllLiveTabs = ({ data }: IProps) => {
+  return <div>AllLiveTabs</div>;
+};
+
+export default AllLiveTabs;
